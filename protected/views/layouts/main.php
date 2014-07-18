6851c52f49dd15340bd0e44320bea48a32ba74c1
Jonas Kick <jonas.kick@web.de>
2014-07-18 11:38:48 +0200

    Footer deleted

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -53,12 +53,6 @@
 
 	<div class="clear"></div>
 
-	<div id="footer">
-		Copyright &copy; <?php echo date('Y'); ?> by My Company.<br/>
-		All Rights Reserved.<br/>
-		<?php echo Yii::powered(); ?>
-	</div><!-- footer -->
-
 </div><!-- page -->
 
 </body>
